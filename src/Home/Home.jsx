135995113f50258717e12fec59f928f69ac2e9b6
MehulBhole <mehulbhole911@gmail.com>
2025-01-05 13:37:32 +0530

    banneradjust

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Banner from "./Banner/Banner";
 import BannerImg from "./Banner/assets/bannerimg.svg"
 import BannerImgMobile from "./Banner/assets/bannerimgmobile.svg"
+import Banner360 from "./Banner/assets/banne360.svg"
 import ConnectUs from "./ConnectUs/ConnectUs";
 import DesignYou from "./DesignYou/DesignYou";
 import LetNumber from "./LetNumber/LetNumber";
@@ -18,6 +19,10 @@ const BannerData =
      image:BannerImgMobile,
      text:"Bring home beautiful interior that fits \n in your budget"
  }
+ const Banner360Mobile={
+     image:Banner360,
+     text:"Bring home beautiful interior that fits \n in your budget"
+ }
 
 
 const DesignYouData={
@@ -25,7 +30,7 @@ const DesignYouData={
     text:"When you give your home the Livspace touch, you get both beauty and functionality. We employ state-of-the-art technology to ensure your home features a flawless look that lasts a very long time"
 }
 const Home = ()=>{
-    const[bannerdata,setBannerData] = useState(window.innerWidth <= 800 ? BannerDataMobile : BannerData);
+    const[bannerdata,setBannerData] = useState(window.innerWidth <= 800 ? window.innerWidth < 480 : Banner360Mobile ? BannerDataMobile : BannerData);
 
     useEffect(() => {
         const handleResize = () => {
@@ -33,7 +38,12 @@ const Home = ()=>{
                 console.log("here");
                 console.log(window.innerWidth);
                 setBannerData(BannerDataMobile);
-            } else {
+            } else if(window.innerWidth < 480){
+                console.log("here480");
+                console.log(window.innerWidth);
+                setBannerData(Banner360Mobile);
+            }
+            else {
                 console.log("here");
                 console.log(window.innerWidth);
                 setBannerData(BannerData);
